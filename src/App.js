91011f6efb028d1a5235a913cbf7cfe73290ae58
Raman01010010 <rmnprjrrr@gmail.com>
2023-07-11 12:26:54 +0530

    components added
    template for componenets added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import data from './data'
+import Corousel from './component/Corousel';
+import NavBar from './component/NavBar';
+import one from './1.jpg'
+import Timeline from './component/Timeline';
+import Speaker from './component/Speakers';
+import About from './component/About';
+import Footer from './component/Footer';
+import Map from './component/Map';
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+     <div>
+      <Corousel/>
+      <About/>
+      <Speaker/>
+      <Timeline/>
+      <Map/>
+    <Footer/>
+      </div>
+
+     
     </div>
   );
 }
---
+++ src/component/Corousel.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+export default function Corousel(){
+    return(
+        <>
+         <Carousel>
+                <div>
+                    <img src="http://www.mnnit.ac.in/images/newstories/2022/slider/yoga2023/DSC_1730.JPG" />
+                    <p>My name is raman</p>
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="http://www.mnnit.ac.in/images/newstories/2022/slider/yoga2023/DSC_1730.JPG" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="http://www.mnnit.ac.in/images/newstories/2022/slider/yoga2023/DSC_1730.JPG" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+        </>
+    )
+}
