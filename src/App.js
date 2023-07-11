81ee4ebf16a5ba12145690d3182368886e976112
Raman01010010 <rmnprjrrr@gmail.com>
2023-07-11 18:30:53 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import data from './data'
+import data from './data/data'
 import Corousel from './component/Corousel';
 import NavBar from './component/NavBar';
 import one from './1.jpg'
