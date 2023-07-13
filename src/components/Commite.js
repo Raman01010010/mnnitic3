c577317b653796ed2982ebcd7aaf288beea5dd6b
Vivek9744 <priyanshusingh202010@gmail.com>
2023-07-13 16:56:33 +0530

    added hover effect in profile
    added hover effect in profile

--- src/components/Commite.js
+++ src/components/Commite.js
@@ -7,8 +7,9 @@ function Commite(props) {
 
 
 
-  <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+  <div className="p-2 lg:w-1/3 md:w-1/2 w-full  ">
+  <div className="transition ease-in-out delay-150 bg-white-500 hover:-translate-y-1 hover:scale-90 hover:bg-indigo-500 duration-300 ...">
+        <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg hover:bg-right-bottom ">
           <img
             alt="team"
             className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
@@ -20,6 +21,7 @@ function Commite(props) {
             </h2>
             <p className="text-gray-600">{props.degre}</p>
           </div>
+        </div>
         </div>
       </div>
     
