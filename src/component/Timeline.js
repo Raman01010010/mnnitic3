c2ae27029443ae2a18d370c93e7c01af7d924bd8
Raman01010010 <rmnprjrrr@gmail.com>
2023-07-13 12:24:10 +0530

    positon fix

--- src/component/Timeline.js
+++ src/component/Timeline.js
@@ -18,7 +18,7 @@ return(
         <div className="h-full w-1 bg-gray-200 pointer-events-none" />
        
       </div>
-      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-2 title-font font-medium text-sm">
         {item.id}
       </div>
       <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
