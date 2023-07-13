dc56b551faffce56098c1b686459e31e52eac8c0
Raman01010010 <rmnprjrrr@gmail.com>
2023-07-13 12:42:23 +0530

    commitee component added

--- src/components/Com.js
+++ src/components/Com.js
@@ -4,10 +4,30 @@ import Comdata from './Comdata.json'
 function Com() {
   return (
     <div>
-        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">TECHNICAL PROGRAM COMMITTEE
-</h1>
-       
-       {
+
+
+
+
+<section className="text-gray-400 bg-gray-900 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-col text-center w-full mb-20">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+        Our Team
+      </h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+        Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+        gentrify, subway tile poke farm-to-table. Franzen you probably haven't
+        heard of them.
+      </p>
+    </div>
+    <div className="flex flex-wrap -m-2">
+
+
+
+      
+
+
+    {
         Comdata.map((element)=>{
             
             return(
@@ -19,6 +39,20 @@ function Com() {
             
         })
        }
+   
+
+
+
+
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
 
     </div>
   )
