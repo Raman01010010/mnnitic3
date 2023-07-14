c9cf64be693cf49d23aba9dc21749c4a64bb3c87
Raman01010010 <rmnprjrrr@gmail.com>
2023-07-14 07:50:42 +0530

    carousel merged from priyansh
    Co-Authored-By: priyanshagra <127022024+priyanshagra@users.noreply.github.com>

--- src/component/C2.js
+++ src/component/C2.js
@@ -1,81 +1,44 @@
+import React from 'react';
+import Slider from 'react-slick';
 
-import React, { useState, useEffect } from 'react';
-import { Transition } from '@headlessui/react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './corousel.css';
 
-const Carousel = ({ images, autoplayDelay = 3000 }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [autoplay, setAutoplay] = useState(true);
-
-  useEffect(() => {
-    let intervalId;
-
-    if (autoplay) {
-      intervalId = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, autoplayDelay);
-    }
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [autoplay, images.length, autoplayDelay]);
-
-  const handleMouseEnter = () => {
-    setAutoplay(false);
-  };
-
-  const handleMouseLeave = () => {
-    setAutoplay(true);
+function Carousel() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   };
 
   return (
-    <div
-      className="relative w-full"
-      style={{ paddingBottom: '70%' }} // Adjust the value to decrease the height of the carousel
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <div className="absolute inset-0">
-        {images.map((image, index) => (
-          <Transition
-            key={index}
-            show={currentIndex === index}
-            enter="transition-opacity duration-1000"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity duration-1000"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <img
-              src={image}
-              alt={`Carousel Image ${index}`}
-              className="absolute top-0 left-0 w-full h-full object-cover"
-            />
-          </Transition>
-        ))}
-      </div>
+    <div className="carousel-container">
+      <Slider {...settings}>
+        <div>
+          <img className="object-contain w-full h-auto"  src="http://www.mnnit.ac.in/images/newstories/2022/slider/yoga2023/DSC_1730.JPG" alt="Image 1" />
+        </div>
+        <div>
+          <img className="object-contain w-full h-auto"  src="http://www.mnnit.ac.in/images/newstories/2022/slider/yoga2023/DSC_1730.JPG" alt="Image 2" />
+        </div>
+        <div>
+          <img className="object-contain w-full h-auto"  src="http://www.mnnit.ac.in/images/newstories/2022/slider/yoga2023/DSC_1730.JPG" alt="Image 3" />
+        </div>
+      </Slider>
     </div>
   );
-};
+}
 
-
-
-const images = [
-  'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?size=626&ext=jpg&ga=GA1.2.2014565303.1683796265&semt=sph',
-  'https://img.freepik.com/free-photo/green-sprouts-dark-soil-against-blurred-background-symbolizing-concept-growth-potential_90220-1462.jpg?size=626&ext=jpg&ga=GA1.2.2014565303.1683796265&semt=sph',
-  'https://img.freepik.com/free-photo/cascade-boat-clean-china-natural-rural_1417-1356.jpg?size=626&ext=jpg&ga=GA1.2.2014565303.1683796265&semt=sph',
-  // Add more image URLs here
-];
-
-const C2= () => {
-  return (
-    <div>
-      
-      <Carousel images={images} autoplayDelay={2000} />
-    </div>
-  );
-};
-
-export default C2;
-
+export default Carousel;
