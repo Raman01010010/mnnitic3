f4aa2b7f6cb3d33a2cf8c4b4777e83aef9c75f62
Raman01010010 <rmnprjrrr@gmail.com>
2023-07-19 08:44:14 +0530

    lite theme

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
 
 
 
-<News/>
+
   
       <About/>
       <Confe/>
--- src/component/Headline.js
+++ src/component/Headline.js
@@ -1,12 +1,12 @@
 export default function Headline(props) {
   return (
     <>
-      <div className="bg-gray-900 flex flex-col">
+      <div className="bg-white flex flex-col">
         <div className="h-1 bg-gray-800 rounded overflow-hidden">
           <div className="w-full h-full bg-indigo-500" />
         </div>
         <div className="flex-wrap sm:flex-row flex-col py-6 mb-4">
-          <h1 className="ml-auto mr-auto sm:w-2/5 text-white font-medium title-font text-2xl mb-2 sm:mb-0  ">
+          <h1 className="ml-auto mr-auto sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0  ">
             {props.text}
           </h1>
          
