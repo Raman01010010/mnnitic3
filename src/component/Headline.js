8695bd35f6448b281322008cedd2c8363660a602
Raman01010010 <rmnprjrrr@gmail.com>
2023-07-19 08:53:33 +0530

    Update Headline.js

--- src/component/Headline.js
+++ src/component/Headline.js
@@ -1,10 +1,8 @@
 export default function Headline(props) {
   return (
     <>
-      <div className="bg-white flex flex-col">
-        <div className="h-1 bg-gray-800 rounded overflow-hidden">
-          <div className="w-full h-full bg-indigo-500" />
-        </div>
+      <div className="bg-gray-200 flex flex-col">
+       
         <div className="flex-wrap sm:flex-row flex-col py-6 mb-4">
           <h1 className="ml-auto mr-auto sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0  ">
             {props.text}
